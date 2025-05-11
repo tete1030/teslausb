675e6d24625d04077be94aecb370c3adc001b7e6
Texot <tete1030@gmail.com>
2025-05-11 16:43:08 +0800

    refactor: replace network interface detection script with udev rule for AP setup

--- setup/pi/configure-ap.sh
+++ setup/pi/configure-ap.sh
@@ -26,7 +26,7 @@ fi
 function nm_get_wifi_client_device () {
   for i in {1..5}
   do
-    WLAN="$(nmcli -t -f TYPE,DEVICE c show --active | grep 802-11-wireless | grep -v ":ap0$" | cut -c 17-)"
+    WLAN="$(nmcli -t -f TYPE,DEVICE d | grep 'wifi:' | grep -v ":ap0$" | cut -d':' -f 2)"
     if [ -n "$WLAN" ]
     then
       break;
@@ -59,6 +59,7 @@ function nm_add_ap () {
 
   # set up access point on the virtual interface using networkmanager
   nmcli con delete TESLAUSB_AP &> /dev/null || true
+
   nmcli con add type wifi ifname ap0 mode ap con-name TESLAUSB_AP ssid "$AP_SSID" || return 1
   # don't set band and channel, because that is controlled by the $WLAN interface
   #nmcli con modify TESLAUSB_AP 802-11-wireless.band bg
@@ -69,19 +70,21 @@ function nm_add_ap () {
   nmcli con modify TESLAUSB_AP ipv4.addr "$IP/24" || return 1
   nmcli con modify TESLAUSB_AP ipv4.method shared || return 1
   nmcli con modify TESLAUSB_AP ipv6.method disabled || return 1
-  cat > /etc/network/if-up.d/teslausb-ap << EOF
+  cat > /root/bin/setup_ap.sh << EOF
 #!/bin/bash
 
-if [ "\$IFACE" = "$WLAN" ]
-then
-  iw dev $WLAN interface add ap0 type __ap
-  iw "$WLAN" set power_save off
-  iw ap0 set power_save off
-  nmcli con up TESLAUSB_AP
-fi
+sleep 2
+
+iw dev $WLAN interface add ap0 type __ap
+iw "$WLAN" set power_save off
+iw ap0 set power_save off
+nmcli con up TESLAUSB_AP
 
 EOF
-  chmod a+x /etc/network/if-up.d/teslausb-ap || return 1
+  chmod a+x /root/bin/setup_ap.sh || return 1
+  echo 'ACTION=="add", SUBSYSTEM=="net", KERNEL=="'$WLAN'", RUN+="/root/bin/setup_ap.sh"' > /etc/udev/rules.d/90-teslausb-ap.rules
+  udevadm control --reload
+
 }
 
 
@@ -89,7 +92,9 @@ if systemctl --quiet is-enabled NetworkManager.service
 then
   # force-install iw because otherwise it will get autoremoved when
   # alsa-utils is removed later
-  apt-get -y --force-yes install iw || return 1
+  apt-get -y --force-yes install iw dnsmasq || return 1
+  systemctl disable dnsmasq
+  systemctl stop dnsmasq
   if ! nm_add_ap
   then
     # Network Manager won't allow adding connections when started with a
